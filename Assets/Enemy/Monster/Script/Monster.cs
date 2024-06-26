8d0ef2b66eacd91dcdf8d8c36760d19e12b0a024
junhyungs <jin05003@naver.com>
2024-06-26 19:26:13 +0900

    [Update]Assets

--- Assets/Enemy/Monster/Script/Monster.cs
+++ Assets/Enemy/Monster/Script/Monster.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class Monster : MonoBehaviour
 {
+    protected MonsterStateMachine m_monsterStateMachine;
+    protected NavMeshAgent m_monsterAgent;
     
+   
+
+
+
+
+
+
+
+
 }
 
 
