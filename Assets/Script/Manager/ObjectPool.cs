b86c44398a0e9e9d36116df8a5d3fd4733e123ad
junHyung <jin05003@naver.com>
2024-09-14 03:31:44 +0900

    [fix] 오브젝트 풀 구조 개선중

---
+++ Assets/Script/Manager/ObjectPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectName
+{
+    PlayerArrow,
+    PlayerFireBall,
+    PlayerHook,
+    PlayerSegment,
+    PlayerBomb,
+    HitEffect,
+
+    MageBullet,
+    OldCrowSegment,
+    GhoulArrow,
+    DekuProjectile,
+
+    Bat,
+    Mage,
+    Ghoul,
+    Deku,
+
+    Soul,
+    UseUI,
+    GetUI,
+    LadderUI
+}
+
+public class Pool
+{
+
+}
+
+public class ObjectPool : Singleton<ObjectPool>
+{
+    
+}
--- Assets/Script/Monster/BT/Deku/DekuBehaviour.cs
+++ Assets/Script/Monster/BT/Deku/DekuBehaviour.cs
@@ -2,17 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DekuBehaviour : MonoBehaviour
+public class DekuBehaviour : BehaviourMonster, IDamged
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("SkinnedMeshRenderer")]
+    [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+    [Header("Material")]
+    [SerializeField] private Material _originalMaterial;
+    [Header("SoulTransform")]
+    [SerializeField] private Transform _soulTransform;
+    [Header("FireTransform")]
+    [SerializeField] private Transform _fireTransform;
+
+    private INode _node;
+    private bool _isDead;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        SetData(MonsterType.Deku);
+        SetMaterial();
+
+        _node = SetBehaviourTree();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!_isDead)
+        {
+            _node.Evaluate();
+        }
+    }
+
+    private void SetMaterial()
+    {
+        _copyMaterial = Instantiate(_originalMaterial);
+        _skinnedMeshRenderer.material = _copyMaterial;
+    }
+    
+    private INode SetBehaviourTree()
+    {
+        return _node;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHp -= damage;
+
+        if(_currentHp <= 0)
+        {
+            _isDead = true;
+
+            Die(_soulTransform);
+        }
+        else
+        {
+            StartCoroutine(IntensityChange(2f, 3f));
+        }
+    }
+
+    public void DekuProjectile()
+    {
+        GameObject bullet = PoolManager.Instance.GetDekuProjectile();
+
+        bullet.transform.position = _fireTransform.position;
+        bullet.transform.rotation = _fireTransform.rotation;
+        DekuBullet bulletComponent = bullet.GetComponent<DekuBullet>();
+        bulletComponent.IsFire(true);
     }
 }
