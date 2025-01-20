774030b37b0c52fc29f6ecb84bfb054bc326182e
junhyungs <jin05003@naver.com>
2025-01-21 02:32:51 +0900

    [feat] 심심해서 만들어본 깨지는 호리병.
    병이 깨지면 5초뒤에 원래 모습으로 돌아감. (그냥 만들어봄...)

---
+++ Assets/Script/Object/POT.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POT : MonoBehaviour, IDamaged
+{
+    private List<POT_Cell> _childList;
+    private HashSet<POT_Cell> _cells;
+
+    private float _health = 1f;
+    private bool _isReady;
+
+    private void Awake()
+    {
+        _cells = new HashSet<POT_Cell>();
+
+        _childList = new List<POT_Cell>();  
+
+        foreach(Transform child in transform)
+        {
+            _childList.Add(child.GetComponent<POT_Cell>());
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(_health <= 0f)
+        {
+            return;
+        }
+
+        _health -= damage;
+
+        foreach (var cell in _childList)
+        {
+            cell.SetKinematic(false);
+        }
+
+        StartCoroutine(ReturnTransform());
+    }
+
+    private IEnumerator ReturnTransform()
+    {
+        _isReady = false;
+
+        yield return new WaitForSeconds(5f);
+
+        foreach(var cell in _childList)
+        {
+            RegisterHashSet(cell);
+
+            StartCoroutine(cell.MoveAndRotateTowardsCoroutine(this));
+        }
+
+        yield return new WaitUntil(() =>
+        {
+            return _isReady;
+        });
+
+        _health = 1f;
+    }
+
+    private void RegisterHashSet(POT_Cell cell)
+    {
+        if (!_cells.Contains(cell))
+        {
+            _cells.Add(cell);
+        }
+    }
+
+    public void UnRegisterHashSet(POT_Cell cell)
+    {
+        if (_cells.Contains(cell))
+        {
+            _cells.Remove(cell);
+
+            if( _cells.Count == 0)
+            {
+                _isReady = true;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Object/POT_Cell.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POT_Cell : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    private Vector3 _initializeLocalPosition;
+    private Quaternion _initializeLocalRotation;
+
+    private float _speed = 5f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        SetKinematic(true);
+
+        _initializeLocalPosition = transform.localPosition;
+        _initializeLocalRotation = transform.localRotation;
+    }
+
+    public void SetKinematic(bool use)
+    {
+        _rigidbody.isKinematic = use;
+    }
+
+    public IEnumerator MoveAndRotateTowardsCoroutine(POT pot)
+    {
+        SetKinematic(true);
+
+        while (true)
+        {
+            float distance = Vector3.Distance(transform.localPosition, _initializeLocalPosition);
+            float angle = Quaternion.Angle(transform.localRotation, _initializeLocalRotation);
+
+            bool isDistance = distance < 0.1f;
+            bool isAngle = angle < 0.1f;
+
+            if (!isDistance)
+            {
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition,
+                _initializeLocalPosition, _speed * Time.deltaTime);
+            }
+
+            if (!isAngle)
+            {
+                float rotationSpeed = _speed * (angle / distance); 
+
+                transform.localRotation = Quaternion.RotateTowards(transform.localRotation,
+                _initializeLocalRotation, rotationSpeed * Time.deltaTime);
+            } 
+
+            if(isDistance && isAngle)
+            {
+                transform.localPosition = _initializeLocalPosition;
+                transform.localRotation = _initializeLocalRotation;
+
+                pot.UnRegisterHashSet(this);
+
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+}
